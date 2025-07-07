b8bde7332d0c03f2dc29e444abced2ccdf6b3643
Artem <Artemkamoshna@gmail.com>
2025-07-07 15:31:09 +0300

    перевірка віку

---
+++ StudentInfo/src/main/java/com/StudentInfo/Main.java
@@ -0,0 +1,56 @@
+package com.StudentInfo;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+  //  public static void main(String[] args) {
+        //Змінні
+//        String name = "Artem";
+//      int age = 29;
+//        double AverageBal = 10.5;
+//        boolean Acyive = true;
+//
+//        System.out.println("Ім'я:" + name);
+//        System.out.println("Вік:" + age);
+//        System.out.println("Середній бал:" + AverageBal);
+//        System.out.println("Актив:" + Acyive);
+
+        // змінні з оцінками
+//        int Number1 = 7;
+//        int Number2 = 9;
+//
+//
+//        int sum = Number1 + Number2;
+//        int difference = Number1 - Number2;
+//        int product = Number1 * Number2;
+//        double share = (double) Number1 / Number2;
+//
+//        // Обчислення
+//        System.out.println("Сума:" + sum);
+//        System.out.println("Різниця:" + difference);
+//        System.out.println("Добуток:" + product);
+//        System.out.println("Чачтка:" + share);
+
+
+//      перевірка віку
+
+//        public static void main(String[] args) {
+//            int age = 29;
+//
+//            if (age < 18) {
+//                System.out.println("Доступ заборонено");
+//            } else {
+//                System.out.println("Доступ дозволено");
+//
+            }
+
+
+
+
+
+
+
+
+
