5cc0c8fa35998374430b77d3f8271168d0e683c0
Artem <Artemkamoshna@gmail.com>
2025-07-27 12:55:18 +0300

    dz7

--- StudentInfo/src/main/java/com/StudentInfo/Calculator.java
+++ StudentInfo/src/main/java/com/StudentInfo/Calculator.java
@@ -9,6 +9,7 @@ public class Calculator {
 
     public static void main(String[] args){
             // Створення об'єкта калькулятора
+
             Calculator calculator = new Calculator();
 
             // Виклик методу add і збереження результату
